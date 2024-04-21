bc5107dd69198520182901539c88b58d79248293
DineshMadderla <133658246+DineshMadderla@users.noreply.github.com>
2024-04-21 14:04:52 +0530

    Add files via upload

---
+++ longestWord.js
@@ -0,0 +1,16 @@
+
+
+const findlongestWord = (string) => {
+  if (string.trim().length === 0) {
+    return false;
+  }
+  stringArray = string.split(" ");
+    stringArray = stringArray.sort((a,b) => b.length - a.length);
+    console.log(stringArray)
+    return stringArray[0]
+  return stringArray.reduce((acc, cur) => (cur.length > acc.length ? cur : acc), "");
+};
+
+console.log(
+  findlongestWord("Hello i'm practicing the javascript logic building")
+);
